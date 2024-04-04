e291ba26e495f490250e05a74e229ef79ce0a47a
AragonSprt <113137975+AragonSprt@users.noreply.github.com>
2024-04-05 00:37:52 +0100

    Add files via upload

--- qr.py
+++ qr.py
@@ -2,8 +2,17 @@ import pyqrcode
 import os
 import shutil
 
+
+def clear():
+    os.system("cls")
+
+
+clear()
 title = input("Give your QR code a title ! >> ")
+print()
 text = input("What would you like the QR code to say >> ")
+print()
+input("Your qr code has been created on your deesktop")
 
 file_name_svg = title + ".svg"
 
@@ -11,7 +20,7 @@ url = pyqrcode.create(text)
 
 url.svg(file_name_svg, scale=8)
 
-os.mkdir(fr"c:\Users\Cyril_\Desktop\{title}")
+os.mkdir(fr"#YOUR PATH WHERE YOU WANT THE FILE\{title}")
 
 
-shutil.move(file_name_svg, fr"c:\Users\Cyril_\Desktop\{title}")
+shutil.move(file_name_svg, fr"#YOUR PATH WHERE YOU WANT THE FILE\{title}")
